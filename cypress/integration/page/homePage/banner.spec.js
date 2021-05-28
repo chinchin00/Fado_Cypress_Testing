d19965063482d1fd32885feff1eaae5f052b090a
trinh699 <trinhcntt99@gmail.com>
2021-05-28 17:18:50 +0700

    first commit

---
+++ cypress/integration/page/homePage/banner.spec.js
@@ -0,0 +1,13 @@
+/// <reference types="Cypress" />
+
+describe('CHECKN BANNER', () => {
+    beforeEach(function (){
+        cy.visitHomePage()
+
+        cy.closePopup()
+    })
+
+    it('Visit banner 1', function() {
+        
+    })
+})
---
+++ cypress/integration/pageObjects/homePage/banner.js
@@ -0,0 +1,9 @@
+class Banner{
+    countHomeSlider(){
+        const el = cy.get('.home-page > #home-slider-section > #home-slider-section__swiper-container > .home-slider-pagination-nav > .swiper-pagination-bullet')
+    }
+    
+    
+}
+
+export default Banner;
